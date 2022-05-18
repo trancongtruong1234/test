52e7281dba471e30868c0df7311adb587e312ea6
trancongtruong1234 <92919801+trancongtruong1234@users.noreply.github.com>
2022-05-18 17:41:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+def get_nameFile_current():
+    import datetime
+    #File dữ liệu được lấy là file trước ngày hiện tại 1 ngày
+    Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+    Previous_Date = Previous_Date.strftime("%m-%d-%Y")
+    print(Previous_Date)
+    name_File = Previous_Date + ".csv"
+    return (name_File)
+def importCsvIntoMySQL(url,table_name):
+
+    import pandas as pd
+    from sqlalchemy import create_engine, types
+
+    df = pd.read_csv(url, index_col=False)
+    print(df.head())
+    engine = create_engine(
+        'mysql://admin:tranduy2906@doan.c3ftrgdgjym4.us-east-1.rds.amazonaws.com/DATA')  # enter your password and database names here
+
+    df.to_sql(table_name, con=engine, index=False, if_exists='append')  # Replace Table_name with your sql table name
+
+if __name__ == '__main__':
+    file_name=get_nameFile_current()
+    # cập nhật dữ liệu vào database thế giới
+    url_world='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+file_name
+    importCsvIntoMySQL(url_world,'COVID_19')
+    # Cập nhật dữ liệu vào database US
+    url_us = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/' + file_name
+    importCsvIntoMySQL(url_us, 'data_covid_us')
+
